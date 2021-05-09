ce3bd680d0c0d189e4af959092a27cd82db039ea
Paolasoares <paolaalcantara1@yahoo.com.br>
2021-05-08 21:26:20 -0400

    Primeiro commit

--- Conversor_idade_dog/ViewController.swift
+++ Conversor_idade_dog/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  Conversor_idade_dog
-//
-//  Created by user194398 on 03/05/21.
-//
+
 
 import UIKit
 
@@ -11,9 +6,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        
 
+    }
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        var idadeCachorro = Int  (campoIdadeCachorro.text!)!
+        idadeCachorro = idadeCachorro * 7
+        
+        legendaResultado.text = "A idade do cachorro e: " + String ( idadeCachorro )
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+            
+    }
 
 }
 
